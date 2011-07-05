48dd310a2e671171183c7a7ae55d973bd0667768
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2011-07-05 19:41:01 -0300

    Concluída a classe do ator Master

--- src/main/java/br/com/insite/test_akka/actors/Master.java
+++ src/main/java/br/com/insite/test_akka/actors/Master.java
@@ -1,16 +1,43 @@
 package br.com.insite.test_akka.actors;
 
 import static akka.actor.Actors.actorOf;
+import static akka.actor.Actors.poisonPill;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.log4j.Logger;
+
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
+import akka.routing.Routing.Broadcast;
+import br.com.insite.test_akka.messages.InitMessage;
+import br.com.insite.test_akka.messages.ResultMessage;
+import br.com.insite.test_akka.messages.WorkMessage;
 
 public class Master extends UntypedActor {
 
-	public Master(int numberOfWorkers, int nrOfMessages, int nrOfElements) {
+	private static final Logger log = Logger.getLogger(Master.class);
+
+	private final int numberOfMessages;
+	private final int numberOfElements;
+	private final CountDownLatch latch;
+
+	private double pi;
+	private int numberOfResults;
+	private long startTime;
+
+	private ActorRef router;
+
+	public Master(int numberOfWorkers, int numberOfMessages,
+			int numberOfElements, CountDownLatch latch) {
+
+		this.numberOfMessages = numberOfMessages;
+		this.numberOfElements = numberOfElements;
+		this.latch = latch;
 
 		final ActorRef[] workers = createWorkers(numberOfWorkers);
 
-		ActorRef router = actorOf(new WorkerFactory(workers)).start();
+		router = actorOf(new WorkerFactory(workers)).start();
 
 	}
 
@@ -25,8 +52,44 @@ public class Master extends UntypedActor {
 	@Override
 	public void onReceive(Object message) throws Exception {
 
+		if (message instanceof InitMessage) {
+			
+			for (int startNumber = 0; startNumber < numberOfMessages; startNumber++) {
+				router.sendOneWay(new WorkMessage(startNumber, numberOfElements), getContext());
+			}
+			
+			router.sendOneWay(new Broadcast(poisonPill()));
+			router.sendOneWay(poisonPill());
+		}
+
+		else if (message instanceof ResultMessage) {
+			
+			ResultMessage result = (ResultMessage) message;
+			pi += result.getValue();
+			numberOfResults += 1;
+
+			if (numberOfResults == numberOfMessages)
+				getContext().stop();
+		}
+
+		else {
+			throw new IllegalArgumentException("Unknown message [" + message + "]");
+		}
+
 	}
 
-	
+	@Override
+	public void preStart() {
+		startTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public void postStop() {
+
+		log.info(String.format(
+				"\n\tEstimativa PI: \t\t%s\n\tCalculation time: \t%s millis",
+				pi, (System.currentTimeMillis() - startTime)));
+		latch.countDown();
+	}
 
 }
